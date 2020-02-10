c1f3bde11b1e8297eb369e223b0c0601f09dd493
Mike Rembo <rembomike@gmail.com>
2020-02-10 17:09:56 +0300

    first commit in opp

---
+++ odongopp.js
@@ -0,0 +1,72 @@
+
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age:20,
+    height:'160m',
+    weight:'68kgs'
+
+  };
+  
+ console.log(person.firstName)
+ console.log(person.lastName)
+ console.log(person.age)
+ console.log(person.height)
+ console.log(person.weight)
+
+
+ let car={
+     make:'Ford',
+     model:'Mustang',
+     year:'1996',
+     engine:'wenkal',
+     tyre:'mud tyre'
+}
+ console.log(car.make)
+ console.log(car.model)
+ console.log(car.year)
+ console.log(car.engine)
+ console.log(car.tyre)
+
+ let school = {
+    name : "Vivekananda School",
+    location : "Delhi",
+    established : "1971",
+    population:1459,
+    teacher:23,
+}
+console.log(school.name)
+console.log(school.location)
+console.log(school.established)
+console.log(school.population)
+console.log(school.teacher)
+
+let dog = {
+Gestation_period: '58 days',
+Family: 'Canidae',
+Lifespan: '13 years',
+Order: 'Carnivora',
+Kingdom: 'Animalia'
+}
+
+console.log(dog.Gestation_period)
+console.log(dog.Family)
+console.log(dog.Lifespan)
+console.log(dog.Order)
+console.log(dog.Kingdom)
+
+
+let house={
+    floor:'tiled',
+    windos:10,
+    doors:5,
+    roof:'iron_sheets',
+    power:'solar'
+}
+
+console.log(house.floor)
+console.log(house.windos)
+console.log(house.doors)
+console.log(house.roof)
+console.log(house.power)
+
