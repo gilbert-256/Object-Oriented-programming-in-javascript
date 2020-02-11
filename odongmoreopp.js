11b1632b0fb8c3073bf2c2a0bb9b31990d7f0a29
Odong Gilbert <47414230+gilbert-256@users.noreply.github.com>
2020-02-11 18:06:32 +0300

    Add files via upload
    more in opp

---
+++ odongmoreopp.js
@@ -0,0 +1,109 @@
+/**
+ * function continent(){
+    this.name="Africa"
+    this.num_of_countries=53
+    this.surrounding_oceans=4
+    this.area="30.7 million km"
+    this.largest_city="Lagos"
+    this.main_river="Nile"
+
+}
+/**Creating an instance  of the defined object called continent 
+
+var europe=new continent()
+europe.name="europe"
+europe.no_ocean=2
+
+console.log(europe.area)
+
+ */
+
+
+
+
+ /**In all the work below,fuctions are defined with properties,instaciate different objects to the defined functions and then printed as log */
+function school(){
+    this.no_of_teachers
+    this.no_of_pupils
+    this.no_of_cooks
+    this.no_classrooms
+}
+console.log(school)
+var lifeline=new school()
+lifeline.no_of_teachers=15
+lifeline.no_of_pupils=3780
+lifeline.no_cooks=30
+lifeline.no_classrooms=16
+console.log(lifeline)
+
+
+function supermarket(){
+    this.no_of_workers
+    this.no_of_products
+    this.size
+    this.location
+    this.cameras
+}
+console.log(supermarket)
+
+var shoprite=new supermarket()
+shoprite.no_of_workers=70
+shoprite.no_of_products=7300
+shoprite.size='4900 square metres'
+this.location='bugolobi'
+this.cameras='cctv'
+
+
+
+function hospital(){
+    this.no_of_patients
+    this.location
+    this.no_of_nurses
+    this.no_of_wards
+    this.no_of_askaris
+}
+console.log(hospital)
+
+var naguru=new hospital()
+naguru.no_of_patients=1300
+naguru.lifeline='bugolobi'
+naguru.no_of_nurses=23
+naguru.no_of_wards=5
+naguru.no_of_askaris=3
+console.log(naguru)
+
+
+function bar(){
+    this.no_of_beer_brands
+    this.no_of_customers
+    this.location
+    this.pizza_price
+    this.no_of_swimming_pools
+}
+console.log(bar)
+
+var mugiez=new bar()
+mugiez.no_of_beer_brands=13
+mugiez.no_of_customers=400
+mugiez.location='mutungo'
+mugiez.pizza_price=30000
+mugiez.no_of_swimming_pools=62
+console.log(mugiez)
+
+
+function mortocycle(){
+    this.no_of_mirrors
+    this.no_of_tyres
+    this.no_engines
+    this.no_of_bulbs
+    this.no_of_fueltanks
+}
+console.log(mortocycle)
+
+var bajaj=new mortocycle()
+bajaj.no_of_mirrors=2
+bajaj.no_of_tyres=2
+bajaj.no_engines=1
+bajaj.no_of_bulbs=6
+bajaj.no_of_fueltanks=2
+console.log(bajaj)
